7040d4ac61111f180d07ba59e5a10fbb8190e8a3
Jacky Huang <lashhw81@gmail.com>
2023-02-13 15:52:10 +0800

    add `serialize`/`deserialize`

--- utility.hpp
+++ utility.hpp
@@ -21,4 +21,31 @@ void update_bbox(bvh::Bvh<float> &bvh, int idx) {
     bvh.nodes[idx].bounding_box_proxy().extend(bvh.nodes[right_idx].bounding_box_proxy());
 }
 
+void serialize(const bvh::Bvh<float> &bvh,
+               const std::vector<bvh::Triangle<float>> &triangles,
+               const char *file_path) {
+    std::ofstream out_file(file_path, std::ios::out | std::ios::binary);
+    out_file.write(reinterpret_cast<const char*>(&bvh.node_count), sizeof(size_t));
+    size_t triangles_size = triangles.size();
+    out_file.write(reinterpret_cast<const char*>(&triangles_size), sizeof(size_t));
+    out_file.write(reinterpret_cast<const char*>(bvh.nodes.get()), sizeof(bvh::Bvh<float>::Node) * bvh.node_count);
+    out_file.write(reinterpret_cast<const char*>(bvh.primitive_indices.get()), sizeof(size_t) * triangles_size);
+    out_file.write(reinterpret_cast<const char*>(triangles.data()), sizeof(bvh::Triangle<float>) * triangles_size);
+}
+
+void deserialize(bvh::Bvh<float> &bvh,
+                 std::vector<bvh::Triangle<float>> &triangles,
+                 const char *file_path) {
+    std::ifstream in_file(file_path, std::ios::in | std::ios::binary);
+    in_file.read(reinterpret_cast<char*>(&bvh.node_count), sizeof(size_t));
+    size_t triangles_size;
+    in_file.read(reinterpret_cast<char*>(&triangles_size), sizeof(size_t));
+    triangles.resize(triangles_size);
+    bvh.nodes = std::make_unique<bvh::Bvh<float>::Node[]>(bvh.node_count);
+    in_file.read(reinterpret_cast<char*>(bvh.nodes.get()), sizeof(bvh::Bvh<float>::Node) * bvh.node_count);
+    bvh.primitive_indices = std::make_unique<size_t[]>(triangles_size);
+    in_file.read(reinterpret_cast<char*>(bvh.primitive_indices.get()), sizeof(size_t) * triangles_size);
+    in_file.read(reinterpret_cast<char*>(triangles.data()), sizeof(bvh::Triangle<float>) * triangles_size);
+}
+
 #endif //BVH_MIXED_OPTIMIZER_UTILITY_HPP
